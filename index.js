b8b83b0d5365de4ba424d58c14507b0d1d87e1bb
zjz2022 <zjz15019702849@gmail.com>
2022-10-26 14:21:21 +0800

    更新说明

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+// 加入線上人數計數
+let onlineCount = 0;
+
+// 修改這一部分
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// 修改 connection 事件
+io.on('connection', (socket) => {
+    // 有連線發生時增加人數
+    onlineCount++;
+    // 發送人數給網頁
+    io.emit("online", onlineCount);
+
+    socket.on("greet", () => {
+        socket.emit("greet", onlineCount);
+    });
+
+    socket.on('disconnect', () => {
+        // 有人離線了，扣人
+        onlineCount = (onlineCount < 0) ? 0 : onlineCount -= 1;
+        io.emit("online", onlineCount);
+    });
+
+    //...
+
+    // 修改 send 事件監聽器
+    socket.on("send", (msg) => {
+        // 如果 msg 內容鍵值小於 2 等於是訊息傳送不完全
+        // 因此我們直接 return ，終止函式執行。
+        if (Object.keys(msg).length < 2) return;
+
+        // 廣播訊息到聊天室
+        io.emit("msg", msg);
+    });
+
+
+});
+
+
+
+// 注意，這邊的 server 原本是 app
+server.listen(3000, () => {
+    console.log("Server Started. http://localhost:3000");
+});
